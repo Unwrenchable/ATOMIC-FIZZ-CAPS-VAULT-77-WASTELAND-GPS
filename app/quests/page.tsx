b6d108988483e92706249ff1e50322d4fdf75bfd
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2025-12-10 02:07:20 -0800

    Add files via upload

---
+++ app/quests/page.tsx
@@ -0,0 +1,9 @@
+﻿export default function QuestsPage() {
+  return (
+    <section>
+      <h1>QUESTS</h1>
+      <p>☑ Restore the Mojave Radio Tower</p>
+      <p>☐ Collect 10 Atomic Fizzcaps</p>
+    </section>
+  );
+}
