9f78d93a9c1c7d8127565908155636fc189cbcf7
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2026-01-01 17:10:58 -0800

    Update loadData.js

--- server/loadData.js
+++ server/loadData.js
@@ -1,36 +1,33 @@
-const fs = require("fs");
-const path = require("path");
+// server/loadData.js
+const fs = require('fs');
+const path = require('path');
 
-function loadJson(filePath) {
+function safeJsonRead(filePath) {
   try {
-    return JSON.parse(fs.readFileSync(filePath, "utf8"));
+    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
   } catch (e) {
-    console.error("JSON load error:", filePath, e);
+    console.error('JSON load error:', filePath, e);
     return [];
   }
 }
 
 function loadAllGameData() {
-  const dataDir = path.join(__dirname, "../public/data");
+  const DATA_DIR = path.join(__dirname, '..', 'data');
 
-  const mintables = loadJson(path.join(dataDir, "mintables.json"));
-  const events = loadJson(path.join(dataDir, "events.json"));
-  const eventLootTables = loadJson(path.join(dataDir, "eventLootTables.json"));
-  const quests = loadJson(path.join(dataDir, "quests.json"));
-  const locations = loadJson(path.join(dataDir, "locations.json"));
+  const locations = safeJsonRead(path.join(DATA_DIR, 'locations.json'));
+  const quests = safeJsonRead(path.join(DATA_DIR, 'quests.json'));
+  const mintables = safeJsonRead(path.join(DATA_DIR, 'mintables.json'));
+  const events = safeJsonRead(path.join(DATA_DIR, 'events.json'));
+  const eventLootTables = safeJsonRead(path.join(DATA_DIR, 'eventLootTables.json'));
+  const recipes = safeJsonRead(path.join(DATA_DIR, 'recipes.json')); // NEW
 
   return {
-    mintables,
-    events,
-    eventLootTables,
-    quests,
-    locations,
-
-    mintablesById: new Map(mintables.map(i => [i.id, i])),
-    eventsById: new Map(events.map(e => [e.id, e])),
-    lootTablesById: new Map(eventLootTables.map(t => [t.id, t])),
-    questsById: new Map(quests.map(q => [q.id, q])),
-    locationsById: new Map(locations.map(l => [l.id, l])),
+    locations: locations,
+    quests: quests,
+    mintables: mintables,
+    events: events,
+    eventLootTables: eventLootTables,
+    recipes: recipes // NEW
   };
 }
 
