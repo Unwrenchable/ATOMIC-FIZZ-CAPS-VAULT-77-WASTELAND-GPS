18c9474d7a45dcbc071f16a2547a05cd3b7f43ae
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2026-01-10 08:46:09 -0800

    Create redis.js

---
+++ backend/redis.js
@@ -0,0 +1,12 @@
+// backend/redis.js
+const Redis = require("ioredis");
+
+const redis = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: 3,
+  enableReadyCheck: true,
+});
+
+redis.on("connect", () => console.log("[redis] connected"));
+redis.on("error", (err) => console.error("[redis] error", err));
+
+module.exports = redis;
