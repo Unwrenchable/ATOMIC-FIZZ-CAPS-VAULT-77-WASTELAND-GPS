3baa4d9a284ae8bb8c8b5852c921a1b56fbd72ee
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2026-01-10 17:50:20 -0800

    Create index.js

---
+++ public/js/overseer/index.js
@@ -0,0 +1,11 @@
+// public/js/overseer/index.js
+
+import "./core.personality.js";
+import "./core.memory.js";
+import "./core.lore.js";
+import "./core.faction.js";
+import "./core.threat.js";
+import "./core.weather.js";
+
+import "./overseer.full.js";
+import "./overseer.js";
