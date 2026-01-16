4d059b6bb95ceade17a1ef990532d62ed754a041
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2026-01-16 12:51:05 -0800

    Update radioPlayer.js

--- public/js/radioPlayer.js
+++ public/js/radioPlayer.js
@@ -1,3 +1,33 @@
+class AtomicFizzRadio {
+  constructor(playlistUrl, stationUrl) {
+    this.audio = new Audio();
+    this.audio.volume = 0.7;
+    this.playlist = [];
+    this.station = {};
+    this.currentTrack = 0;
+
+    Promise.all([
+      fetch(playlistUrl).then(r => r.json()),
+      fetch(stationUrl).then(r => r.json())
+    ]).then(([playlist, station]) => {
+      this.playlist = playlist.tracks;
+      this.station = station;
+      this.playRandom();
+    });
+  }
+
+  playRandom() {
+    this.currentTrack = Math.floor(Math.random() * this.playlist.length);
+    const track = this.playlist[this.currentTrack];
+    this.audio.src = `/audio/radio/tracks/${track.file}`;
+    this.audio.play();
+
+    this.audio.onended = () => this.playRandom();
+  }
+}
+
+window.AtomicFizzRadio = AtomicFizzRadio;
+
 {
   "station": "Atomic Fizz Radio",
   "description": "Vintage swing, jazz, western, and novelty tunes for the wandering survivor.",
