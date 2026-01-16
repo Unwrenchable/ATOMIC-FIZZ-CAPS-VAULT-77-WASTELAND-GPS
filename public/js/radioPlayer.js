97729e85ed02f702db98fb9d8f6c4175dd085408
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2026-01-16 12:49:09 -0800

    Create radioPlayer.js

---
+++ public/js/radioPlayer.js
@@ -0,0 +1,15 @@
+{
+  "station": "Atomic Fizz Radio",
+  "description": "Vintage swing, jazz, western, and novelty tunes for the wandering survivor.",
+  "tracks": [
+    { "file": "just_swing.mp3", "title": "Just Swing", "artist": "Big Band Ensemble" },
+    { "file": "satin_shoes_and_brass.mp3", "title": "Satin Shoes and Brass", "artist": "Vintage Orchestra" },
+    { "file": "lazy_bones.mp3", "title": "Lazy Bones", "artist": "Billy Munn" },
+    { "file": "backseat_bop.mp3", "title": "Backseat Bop", "artist": "Rockabilly Trio" },
+    { "file": "poodle_skirt_swirl.mp3", "title": "Poodle Skirt Swirl", "artist": "Retro Dance Band" },
+    { "file": "home_on_the_range.mp3", "title": "Home on the Range", "artist": "US Army Band" },
+    { "file": "shenandoah.mp3", "title": "Shenandoah", "artist": "US Army Chorus" },
+    { "file": "stars_and_stripes.mp3", "title": "Stars and Stripes Forever", "artist": "US Marine Band" },
+    { "file": "retro_radio_jingle.mp3", "title": "Retro Radio Jingle", "artist": "Vintage Novelty" }
+  ]
+}
